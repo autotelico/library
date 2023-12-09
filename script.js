056de391966ceabca523fe44da1136ed9dbd6b1b
Henrique Heron <henriqueheron1@gmail.com>
2023-12-09 12:51:12 -0300

    add list of inputs for user to insert data into

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const usersBook = document.querySelector('#users-book');
+    const bookAuthor = document.querySelector('#book-author');
+    const bookTitle = document.querySelector('#book-title');
+    const bookPageCount = document.querySelector('#book-page-count');
+    const hasRead = document.querySelector('#book-has-read');
+
+    const cardTitle = document.querySelector('.book-title');
+    const cardDescription = document.querySelector('.book-description');
     // const form = document.querySelector('form');
     const addButton = document.querySelector('#add-button');
     const resetButton = document.querySelector('#reset-button');
 
+
     const myLibrary = [];
 
-    function Book() {
-        // will build books here
+    function Book(title, description) {
+        this.title = title;
+        this.description = description;
     }
 
     function addBook(book) {
@@ -16,18 +24,25 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log(myLibrary);
     }
 
+
     usersBook.addEventListener('keydown', (e) => {
         if (e.key === 'Enter') {
             addBook(usersBook.value);
+            usersBook.value = '';
         }
     })
-
-    resetButton.addEventListener('click', () => {
-        while (myLibrary.length > 0) {
-            myLibrary.pop();
-            console.log(myLibrary);
-        }
-    })
-
-
+    
+    
+    const theHobbit = new Book('The Hobbit', 'Cool book by J.R.R Tolkien');
+    
+    bookTitle.textContent = theHobbit.title;
+    bookDescription.textContent = theHobbit.description;
+    
 })
+
+// resetButton.addEventListener('click', () => {
+//     while (myLibrary.length > 0) {
+//         myLibrary.pop();
+//         console.log(myLibrary);
+//     }
+// })   
