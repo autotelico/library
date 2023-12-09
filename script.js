6076053aea47d9834ee7d97afc08ccb311dec302
Henrique Heron <henriqueheron1@gmail.com>
2023-12-09 20:35:48 -0300

    create cards with object values and display them

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+
     const bookAuthor = document.querySelector('#book-author');
     const bookTitle = document.querySelector('#book-title');
     const bookPages = document.querySelector('#book-pages');
     const bookReadStatus = document.querySelector('#book-has-read');
+
+    const mainContainer = document.querySelector('#main-container');
 
     const cardAuthor = document.querySelector('.card-author');
     const cardTitle = document.querySelector('.card-title');
@@ -16,7 +19,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
     cardAuthor.textContent = bookAuthor.value;
 
+
+    function Book(author, title, pages, hasRead) {
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
+        this.hasRead = Boolean(hasRead);
+    }
+
+    const theHobbit = new Book('JRR Tolkien', 'The Hobbit', 300, true);
+    const harryPotter = new Book('JK Rowling', 'Harry Potter', 10, false);
+    const gameOfThrones = new Book('George RR Martin', 'Game of Thrones', 1000, true);
+
     const myLibrary = [];
+    console.log(myLibrary);
 
     function addBookToLibrary(author, title, pages, readStatus) {
         let builtBook = {
@@ -27,17 +43,56 @@ document.addEventListener("DOMContentLoaded", () => {
         };
         myLibrary.push(builtBook);
         console.log(myLibrary);
-        displayInfo();
+        // displayInfo();
+        displayBooks();
     }
 
-    function displayInfo() {
-        for (const item of myLibrary) {
-            cardAuthor.textContent = item.author;
-            cardTitle.textContent = item.title;
-            cardPages.textContent = item.pages;
-            cardReadStatus.textContent = item.readStatus;
+    function displayBooks() { // fancier displayInfo() sibling
+
+        clearOldCards();
+
+        for (const book of myLibrary) {
+
+            const newCard = document.createElement('div');
+            newCard.classList.add('card', 'container', 'border', 'border-secondary', 'bg-warning', 'm-2', 'p-3');
+            mainContainer.appendChild(newCard);
+
+            const newAuthor = document.createElement('p');
+            newCard.appendChild(newAuthor);
+            newAuthor.textContent = book.author;
+
+            const newTitle = document.createElement('p');
+            newCard.appendChild(newTitle);
+            newTitle.textContent = book.title;
+
+            const newPages = document.createElement('p');
+            newCard.appendChild(newPages);
+            newPages.textContent = book.pages;
+
+            const newReadStatus = document.createElement('p');
+            newCard.appendChild(newReadStatus);
+            newReadStatus.textContent = book.readStatus;
         }
     }
+
+    function clearOldCards() {
+        const cardList = document.querySelectorAll('.card');
+
+        for (const card of cardList) {
+            card.remove();
+        }
+
+    }
+
+    // function displayInfo() {
+
+    //     for (const item of myLibrary) {
+    //         cardAuthor.textContent = item.author;
+    //         cardTitle.textContent = item.title;
+    //         cardPages.textContent = item.pages;
+    //         cardReadStatus.textContent = item.readStatus;
+    //     }
+    // }
 
     function clearInputFields() {
         bookAuthor.value = '';
@@ -52,17 +107,8 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 
 
-    // function Book(author, title, pages, hasRead) {
-    //     this.author = author;
-    //     this.title = title;
-    //     this.pages = pages;
-    //     this.hasRead = Boolean(hasRead);
-    // }
 
 
-    // const theHobbit = new Book('JRR Tolkien', 'The Hobbit', 300, true);
-    // const harryPotter = new Book('JK Rowling', 'Harry Potter', 10, false);
-    // const gameOfThrones= new Book('George RR Martin', 'Game of Thrones', 1000, true);
 
     // for (const book in myLibrary) {
     //     cardAuthor.textContent = bookAuthor.value;
